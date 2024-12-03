a944c3bd0d7bc17e79e68757aee10a6f9f04dcea
himank94 <himankdharmyug22@gmail.com>
2024-12-03 17:27:32 +0530

    updated

--- stockupdates.py
+++ stockupdates.py
@@ -1,8 +1,22 @@
-import random
 import matplotlib.pyplot as plt
+from itertools import count
+from matplotlib.animation import FuncAnimation
+import random
 
-while random!=0:
-    time = random.randint(00,60)
-    data = random.randint(00000,99999)
-plt.plot(time,data)
+# create two list with empty data at initially
+x = []
+y = []
+
+#take the value count every time
+a = count() 
+
+#create live plot graph function
+def livePlotGraph(i):
+    x.append(next(a))
+    y.append(random.randint(0,20))
+    plt.cla
+    plt.plot(x,y)
+    
+#call function n times
+myfunc = FuncAnimation(plt.gcf(), livePlotGraph, interval=500)
 plt.show()
